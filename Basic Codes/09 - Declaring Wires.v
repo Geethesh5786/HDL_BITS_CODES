ba1c3ca1721d6aaa8532a0a4a0aed9e257516339
PABBATHI GEETHESH <geetheshec036@gmail.com>
2024-12-06 07:12:52 +0530

    Basic Codes

--- Basic Codes/09 - Declaring Wires.v
+++ Basic Codes/09 - Declaring Wires.v
@@ -1,16 +1,16 @@
 `default_nettype none
 module top_module(
-    input a,
-    input b,
-    input c,
-    input d,
+    input w,
+    input x,
+    input y,
+    input z,
     output out,
     output out_n   ); 
 	
     wire int1,int2;
     
-    assign int1 = a&b;
-    assign int2 = c&d;
+    assign int1 = w&x;
+    assign int2 = y&z;
     assign out = int1|int2;
     assign out_n = ~out;
     
