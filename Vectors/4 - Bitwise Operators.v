bbd901878d2fc20a1141526ba74699fa78c714b3
PABBATHI GEETHESH <geetheshec036@gmail.com>
2024-12-06 07:20:39 +0530

    Vectors

--- Vectors/4 - Bitwise Operators.v
+++ Vectors/4 - Bitwise Operators.v
@@ -1,14 +1,14 @@
 module top_module( 
-    input [2:0] a,
-    input [2:0] b,
+    input [2:0] x,
+    input [2:0] y,
     output [2:0] out_or_bitwise,
     output out_or_logical,
     output [5:0] out_not
 );
     
-    assign out_or_bitwise = a|b;
-    assign out_or_logical = a||b;
-    assign out_not[5:3]   = ~b; //this is bitwise not
-    assign out_not[2:0]   = ~a;
+    assign out_or_bitwise = x|y;
+    assign out_or_logical = x||y;
+    assign out_not[5:3]   = ~y; 
+    assign out_not[2:0]   = ~x;
 
 endmodule
