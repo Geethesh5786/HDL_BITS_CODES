b85c2bf3a7ccc0508d296af8a942809509dd3cdc
PABBATHI GEETHESH <geetheshec036@gmail.com>
2024-12-06 07:17:42 +0530

    Vectors

--- Vectors/2 - Vectors in more detail.v
+++ Vectors/2 - Vectors in more detail.v
@@ -1,14 +1,8 @@
-`default_nettype none     // Disable implicit nets. Reduces some types of bugs.
-// Implicit nets are always one-bit wires and causes bugs if you had intended to use a vector. 
-//Disabling creation of implicit nets can be done using the `default_nettype none directive.
-
+`default_nettype none    
 module top_module( 
     input wire [15:0] in,
-    output wire [7:0] out_hi,
-    output wire [7:0] out_lo );
-    //The unpacked dimensions are declared after the name. They are generally used to declare memory arrays. 
-	//reg [7:0] mem [255:0];   // 256 unpacked elements, each of which is a 8-bit packed vector of reg.
-    
-    assign out_hi = in[15:8];
-    assign out_lo = in[7:0];
+    output wire [7:0] out_high,
+    output wire [7:0] out_low );
+    assign out_high = in[15:8];
+    assign out_low  = in[7:0];
 endmodule
