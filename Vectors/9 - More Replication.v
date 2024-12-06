15c403e3f06706dae6d397afffbbf54533c090f6
PABBATHI GEETHESH <geetheshec036@gmail.com>
2024-12-06 07:25:15 +0530

    Vectors

--- Vectors/9 - More Replication.v
+++ Vectors/9 - More Replication.v
@@ -1,8 +1,5 @@
 module top_module (
-    input a, b, c, d, e,
-    output [24:0] out );//
-
-    // The output is XNOR of two vectors created by 
-    // concatenating and replicating the five inputs.
-    assign out = ~{ {5{a}},{5{b}},{5{c}},{5{d}},{5{e}}} ^ { {5{a,b,c,d,e}}};
+    input v, w, x, y, z,
+    output [24:0] out ); 
+    assign out = ~{ {5{v}},{5{w}},{5{x}},{5{y}},{5{z}}} ^ { {5{v,w,x,y,z}}};
 endmodule
