fc018a5ebc8105f3563bb3ca09f88f2206387240
PABBATHI GEETHESH <geetheshec036@gmail.com>
2024-12-06 07:22:04 +0530

    Vectors

--- Vectors/6 - Vector Concatanation Operator.v
+++ Vectors/6 - Vector Concatanation Operator.v
@@ -1,11 +1,8 @@
 module top_module (
     input [4:0] a, b, c, d, e, f,
-    output [7:0] w, x, y, z );//
-	//Concatenation needs to know the width of every component (or how would you know the length of the result?). 
-    //Thus, {1, 2, 3} is illegal and results in the error message: unsized constants are not allowed in concatenations.
-	//The concatenation operator can be used on both the left and right sides of assignments.
-    
-    wire [31:0] concat_reg; //raise an error when work with reg. Reason unknown???
+    output [7:0] w, x, y, z );
+	
+    wire [31:0] concat_reg;
     assign concat_reg = {a[4:0], b[4:0], c[4:0], d[4:0], e[4:0], f[4:0], 2'b11};
     assign w = concat_reg[31:24];
     assign x = concat_reg[23:16];
