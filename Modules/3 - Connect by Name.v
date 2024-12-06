931d57d79ee73ee5bf5b59c415e2c95cbf45266c
PABBATHI GEETHESH <geetheshec036@gmail.com>
2024-12-06 07:27:34 +0530

    Modules

--- Modules/3 - Connect by Name.v
+++ Modules/3 - Connect by Name.v
@@ -6,7 +6,7 @@ module top_module (
     output out1,
     output out2
 );
-    //connect ports by name
+    
     mod_a inst_1(.in1(a), .in2(b), .in3(c), .in4(d), .out1(out1), .out2(out2));
 
 endmodule
